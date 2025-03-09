fca400ac0f6b166f4b3fdc99503ea5949f7c77a1
Adevanir <adevaniraleixo1996@gmail.com>
2025-03-09 20:25:06 -0300

    UML CLASES

--- code/game.py
+++ code/game.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import pygame
 class Game:
     def __init__(self):
         self.window = None
 
-    def run(self, ):
-        pass
+def run(self, ):
+
+    print('Setup Start')
+
+pygame.init()
+window = pygame.display.set_mode(size=(800, 600))
+print('Setup End')
+print('Loop Setup')
+while True:
+    ## Checar todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  ##Fechar Janela
+            quit()  ## fechar jogo
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.game import Game
 
-print('Setup Start')
-
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print('Setup End')
-print('Loop Setup')
-while True:
-    ## Checar todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  ##Fechar Janela
-            quit()  ## fechar jogo
+game = Game()
+game.run()
