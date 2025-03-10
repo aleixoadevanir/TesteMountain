5999e6cf850c23fd637633e95337974ab244b7cc
Adevanir <adevaniraleixo1996@gmail.com>
2025-03-09 21:22:06 -0300

    contante Tamanho Imagens

--- code/game.py
+++ code/game.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+from code.menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGT
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGT ))
 
-def run(self, ):
+    def run(self, ):
 
-    print('Setup Start')
-
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print('Setup End')
-print('Loop Setup')
-while True:
-    ## Checar todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  ##Fechar Janela
-            quit()  ## fechar jogo
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+        pass
+            ## Checar todos os eventos
+            ##for event in pygame.event.get():
+            ##    if event.type == pygame.QUIT:
+            ##        pygame.quit()  ##Fechar Janela
+            ##        quit()  ## fechar jogo
--- code/menu.py
+++ code/menu.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
 
-from Menu import Menu
 
+class Menu:
 
-class Menu(Menu):
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load( './assets/MenuBg.png')
+        self.rect = self.surf.get_rect(left = 0, top = 0)
 
     def run(self, ):
+        self.window.blit(source = self.surf, dest= self.rect)
+        pygame.display.flip()
         pass
